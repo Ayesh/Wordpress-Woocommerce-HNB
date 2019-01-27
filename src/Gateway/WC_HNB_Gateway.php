5991589608716dfcc5f3b51c5ba3c7273d22fd5d
Ayesh Karunaratne <ayeshlakmal@gmail.com>
2019-01-28 00:12:29 +0530

    Extract gateway meta data to private constants

--- src/Gateway/WC_HNB_Gateway.php
+++ src/Gateway/WC_HNB_Gateway.php
@@ -10,6 +10,10 @@ use WC_Payment_Gateway;
  *
  * @package     Ayesh/WooCommerceHNB
  */
-class WC_HNB_Gateway extends WC_Payment_Gateway{
+final class WC_HNB_Gateway extends WC_Payment_Gateway {
+	private const NAME = 'HNB Online Payment';
+	private const ID = 'hnb_ipg';
+	private const DESCRIPTION = 'Hatton National Bank Internet Payment Gateway to accept Visa/Master credit and debit cards issued both locally and internationally.';
+	private const IPG_URL = 'https://www.hnbpg.hnb.lk/SENTRY/PaymentGateway/Application/ReDirectLink.aspx';
 
 }
