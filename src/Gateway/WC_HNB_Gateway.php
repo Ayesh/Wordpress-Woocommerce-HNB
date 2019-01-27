4da689fdf4126c153f7cebeb4b1af126bdbae29a
Ayesh Karunaratne <ayeshlakmal@gmail.com>
2019-01-27 23:13:33 +0530

    Add plugin file with base hooks and payment class skeleton

---
+++ src/Gateway/WC_HNB_Gateway.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Ayesh\WooCommerceHNB\Gateway;
+
+use WC_Payment_Gateway;
+
+/**
+ * WooCommerce Payment Gateway integration for Hatton National Bank IPG.
+ *
+ * @package     Ayesh/WooCommerceHNB
+ */
+class WC_HNB_Gateway extends WC_Payment_Gateway{
+
+}
---
+++ woocommerce-hnb.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Ayesh\WooCommerceHNB;
+
+use Ayesh\WooCommerceHNB\Gateway\WC_HNB_Install;
+use Ayesh\WooCommerceHNB\Gateway\WC_HNB_Gateway;
+
+/**
+ * Plugin Name: WooCommerce HNB
+ * Plugin URI: https://wordpress.org/plugins/woocommerce-hnb
+ * Description: WooCommerce Payment Gateway plugin integration with Hatton National Bank Sri Lanka.
+ * Version: 1.0.0
+ * Author: Ayesh Karunaratne
+ * Author URI: https://ayesh.me/open-source
+ * Text Domain:  woocommerce-sampath-bank
+ * WC requires at least: 3.3
+ * WC tested up to: 3.5
+ *
+ * @package WooCommerce\Blocks
+ */
+
+defined( 'ABSPATH' ) || die();
+
+add_action('plugins_loaded', __NAMESPACE__ . '\plugin_loaded', 0);
+add_filter( 'plugin_action_links_woocommerce-hnb/woocommerce-hnb.php', __NAMESPACE__ . '\action_links');
+add_filter('woocommerce_payment_gateways', __NAMESPACE__ . '\payment_gateway');
+
+
+function plugin_loaded() {
+	include_once dirname( __FILE__ ) . '/src/Gateway/WC_HNB_Gateway.php';
+}
+
+function action_links ($links) {
+	array_unshift($links,
+		'<a href="admin.php?page=wc-settings&tab=checkout&section=woocommerce_hnb">Settings</a>');
+	return $links;
+}
+
+function payment_gateway($methods) {
+	$methods[] = WC_HNB_Gateway::class;
+	return $methods;
+}
