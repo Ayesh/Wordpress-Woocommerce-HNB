2068dc9c3e1cb613bdb52b41dcabfff491218024
Ayesh Karunaratne <ayeshlakmal@gmail.com>
2019-01-30 14:19:31 +0530

    Fix typo in error message 'possible forged requests'

--- src/Gateway/WC_HNB_Gateway.php
+++ src/Gateway/WC_HNB_Gateway.php
@@ -263,7 +263,7 @@ final class WC_HNB_Gateway extends WC_Payment_Gateway {
 					wp_redirect($this->get_return_url($order));
 					break;
                 }
-				$order->add_order_note('Payment signature mismatch. This indicates a possible forged requests.');
+				$order->add_order_note('Payment signature mismatch. This indicates a possible forged request.');
 				$order->add_order_note('Payment validation failed.', 1);
 				wc_add_notice(__('Payment validation error. Please try again.', self::TD), 'error');
 				break;
